facd9dd504a4573e6a4286a3462497d75520b185
78Alpha <37676834+78Alpha@users.noreply.github.com>
2018-12-02 12:06:46 -0800

    Updated Main to Python3
    Reduced the file size by deleting useless data

--- Main.py
+++ Main.py
@@ -20,28 +20,21 @@ def word_corex(stub, sen): # Stationary title with a natural typing statement
 
 def intro(): #The intro to the game
     clear()
-    word_core("@@;          #@@  @@@@@@@@@@@@@@@@   .@@@@@@@@@@@@@@  @@@@@@@@@@@@@@+    '@@@@@@@@@@@@`\n", 0.001)
-    word_core("@@@@         #@@  @@@@@@@@@@@@@@@@  ,@@@@@@@@@@@@@@@  @@@@@@@@@@@@@@@@  @@@@@@@@@@@@@@@`\n", 0.001)
-    word_core("@@@@@        #@@  @@@:::::::::::::  @@@@::::::::::::  @@@:::::::::+@@@  @@@+::::::::@@@@\n", 0.001)
-    word_core("@@@@@@       #@@  @@@               @@@               @@@          `@@  @@.          @@@\n", 0.001)
-    word_core("@@.@@@@.     #@@  @@@@@@@@@@@@@@    @@@               @@@          `@@  @@.          @@@\n", 0.001)
-    word_core("@@. ;@@@+    #@@  @@@@@@@@@@@@@@    @@@               @@@          ;@@  @@.          @@@\n", 0.001)
-    word_core("@@.  `@@@@   #@@  @@@@@@@@@@@@@@    @@@               @@@@@@@@@@@@@@@@  @@.          @@@\n", 0.001)
-    word_core("@@.    @@@@  #@@  @@@               @@@               @@@@@@@@@@@@@@@   @@.          @@@\n", 0.001)
-    word_core("@@.     @@@@`#@@  @@@               @@@               @@@   @@@@        @@.          @@@\n", 0.001)
-    word_core("@@.      ;@@@@@@  @@@               @@@               @@@    '@@@+      @@.          @@@\n", 0.001)
-    word_core("@@.       `@@@@@  @@@:::::::::::::  @@@@::::::::::::  @@@     `@@@@     @@@+::::::::@@@@\n", 0.001)
-    word_core("@@.         @@@@  @@@@@@@@@@@@@@@@  ,@@@@@@@@@@@@@@@  @@@       @@@@:   @@@@@@@@@@@@@@@`\n", 0.001)
-    word_core("@@.          @@@  @@@@@@@@@@@@@@@@   .@@@@@@@@@@@@@@  @@@        +@@@@   +@@@@@@@@@@@@.\n", 0.001)
+    word_core("@@;          #@@  @@@@@@@@@@@@@@@@   .@@@@@@@@@@@@@@  @@@@@@@@@@@@@@+    '@@@@@@@@@@@@`\n", 0.0001)
+    word_core("@@@@         #@@  @@@@@@@@@@@@@@@@  ,@@@@@@@@@@@@@@@  @@@@@@@@@@@@@@@@  @@@@@@@@@@@@@@@`\n", 0.0001)
+    word_core("@@@@@        #@@  @@@:::::::::::::  @@@@::::::::::::  @@@:::::::::+@@@  @@@+::::::::@@@@\n", 0.0001)
+    word_core("@@@@@@       #@@  @@@               @@@               @@@          `@@  @@.          @@@\n", 0.0001)
+    word_core("@@.@@@@.     #@@  @@@@@@@@@@@@@@    @@@               @@@          `@@  @@.          @@@\n", 0.0001)
+    word_core("@@. ;@@@+    #@@  @@@@@@@@@@@@@@    @@@               @@@          ;@@  @@.          @@@\n", 0.0001)
+    word_core("@@.  `@@@@   #@@  @@@@@@@@@@@@@@    @@@               @@@@@@@@@@@@@@@@  @@.          @@@\n", 0.0001)
+    word_core("@@.    @@@@  #@@  @@@               @@@               @@@@@@@@@@@@@@@   @@.          @@@\n", 0.0001)
+    word_core("@@.     @@@@`#@@  @@@               @@@               @@@   @@@@        @@.          @@@\n", 0.0001)
+    word_core("@@.      ;@@@@@@  @@@               @@@               @@@    '@@@+      @@.          @@@\n", 0.0001)
+    word_core("@@.       `@@@@@  @@@:::::::::::::  @@@@::::::::::::  @@@     `@@@@     @@@+::::::::@@@@\n", 0.0001)
+    word_core("@@.         @@@@  @@@@@@@@@@@@@@@@  ,@@@@@@@@@@@@@@@  @@@       @@@@:   @@@@@@@@@@@@@@@`\n", 0.0001)
+    word_core("@@.          @@@  @@@@@@@@@@@@@@@@   .@@@@@@@@@@@@@@  @@@        +@@@@   +@@@@@@@@@@@@.\n", 0.0001)
     print("\n")
     word_corex("DEVELOPER |", "78 Alpha\n")
-    #time.sleep(5)
-    #clear()
-    #word_core("Welcome to the apocalypse...\n", 0.01)
-    print("\n")
-    time.sleep(1)
-    word_corex("INIT |", "Creates an initial save")
-    print("\n")
     time.sleep(0.5)
     word_corex("START |", "Start the game")
     print("\n")
@@ -49,10 +42,8 @@ def intro(): #The intro to the game
     word_corex("EXIT |", "Exit the application")
     print("\n")
     time.sleep(0.5)
-    action = raw_input("What would you like to do: ")
-    if action == "INIT":
-        Save_Make()
-    elif action == "START":
+    action = input(str("What would you like to do: "))
+    if action == "START":
         diff_set()
     elif action == "EXIT":
         clear()
@@ -60,27 +51,8 @@ def intro(): #The intro to the game
     else:
         intro()
 
-
-def Save_Make(): # Create a save if none exists, this is necessary to run the game, or delete a previous save
-    clear()
-    global OS
-    if OS == "Windows":
-        os.system("del Save_file.py")
-    else:
-        os.system("rm Save_file.py")
-    with open("Globals.py", "r") as file:
-        read = file.readlines()
-        content = [x.strip() for x in read]
-    with open("Save_file.py", "a") as written:
-        for i in range(0, len(content)):
-            written.write(content[i] + "\n")
-    word_corex("CREATING |", "Save data has been successfully created!")
-    intro()
-
 def diff_set():
     import Level1
-    #start_turn()
-
 
 def clear():
     global OS
@@ -88,6 +60,5 @@ def clear():
         os.system("cls")
     else:
         os.system("clear")
-
 
 intro()
